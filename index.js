eb5e76e04c030883ccc4616e68361f73de14ecd1
Mark Ramsey <mramsey2624@gmail.com>
2017-01-25 11:50:36 -0500

    Added function to call faa data helper

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var app = new Alexa.app('airportinfo');
 
 var FAADataHelper = require('./faa_data_helper');
 
-app.launch(function(req, res) {
+app.launch(function (req, res) {
     //console.log('Hitting launch function');
     var prompt = 'For delay information, tell me an Airport code.';
     //console.log(req);
@@ -20,16 +20,42 @@ app.launch(function(req, res) {
 });
 
 app.intent('airportinfo', {
-    'slots': {
-        'AIRPORTCODE': 'FAACODES'
-    },
+        'slots': {
+            'AIRPORTCODE': 'FAACODES'
+        },
 
-    'utterances': ['{|flight|airport} {|delay|status} {|info} {|for} {|AIRPORTCODE}']
+        'utterances': ['{|flight|airport} {|delay|status} {|info} {|for} {|AIRPORTCODE}']
     },
-    function(req,res) {
+    function (req, res) {
+        //get the slots
+
+        var airportCode = req.slot('AIRPORTCODE');
+
+        var reprompt = 'Tell me an airportcode to get delay information';
+
+        if (_.isEmpty(airportCode)) {
+            var prompt = 'I did\'t hear an airport code. Tell me an airport code.';
+
+            res.say(prompt).reprompt(reprompt).shouldEndSession(false);
+
+            return true;
+        } else {
+            var faaHelper = new FAADataHelper();
+            faaHelper.requestAirportStatus(airportCode).then(function (airportStatus) {
+                console.log(airportStatus);
+                res.say(faaHelper.formatAirportStatus(airportStatus)).send();
+
+            }).catch(function (err) {
+                console.log(err.statusCode);
+
+                var prompt = 'I didn\'t have data for an airport code of ' + airportCode;
+
+                res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
+            });
+            return false;
+        }
 
     }
 );
 
 module.exports = app;
-
